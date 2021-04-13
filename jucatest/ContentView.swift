fa83bc7627d057e1d3f512ddbda12b7a4af86832
Juan Carlos Ricalde Poveda <jucaripo@gmail.com>
2021-04-13 10:44:54 -0500

    Update a la vista para tener el diseño del tutorial

--- jucatest/ContentView.swift
+++ jucatest/ContentView.swift
@@ -9,49 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                VStack(alignment: .leading){
-                    HStack {
-                        Text(Image(systemName: "moon.stars"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Nightly Tasks")
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .underline()
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    // MARK: Nightly Tasks
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(.uppercase)
+                        }
+                        Text("- Check all windows")
+                        Text("- Check all doors")
+                        Text("- Check that the safe is locked")
+                        Text("- Check the mailbox")
+                        Text("- Inspect security cameras")
+                        Text("- Clear ice from sidewalks")
+                        Text("- Document \"strange and unusual\" occurrences")
                     }
-                    Text("Check All Windows")
-                    Text("Check all doors")
-                    Text("Check htat the safe is locked")
-                    Text("Check the mailbox")
-                    Text("Inspect security cameras")
-                    Text("Clear ice from sidewalks")
-                    Text("Document \"Strnge and unusual\" occurences")
-                    HStack {
-                        Text(Image(systemName: "sunset"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Weekly Tasks").font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/).fontWeight(.heavy).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).underline()
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    
+                    // MARK: Weekly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "sunset"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        
+                        Text("- Check inside all vacant rooms")
+                        Text("- Walk the perimeter of property")
                     }
-                    HStack {
-                        Text(Image(systemName: "calendar"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Monthly Tasks").font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/).fontWeight(.heavy).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).underline()
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    // MARK: Monthly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "calendar"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("- Test security alarm")
+                        Text("- Test motion detectors")
+                        Text("- Test smoke alarms")
                     }
                 }
-                .foregroundColor(.gray)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
                 Spacer()
             }
-            .padding([.top,.leading])
+            .padding([.top, .leading])
+            
+            
             Spacer()
         }
     }
