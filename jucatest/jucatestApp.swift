f4895e95d1b245b65c386b5667b8ab972f2377e2
Juan Carlos Ricalde Poveda <jucaripo@gmail.com>
2021-04-12 19:15:13 -0500

    Initial Commit

---
+++ jucatest/jucatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  jucatestApp.swift
+//  jucatest
+//
+//  Created by Juan Carlos Ricalde Poveda on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct jucatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
