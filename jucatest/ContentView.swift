b6b50b14cec79c21fc5d69826de901fb567f9f73
Juan Carlos Ricalde Poveda <jucaripo@gmail.com>
2021-04-12 20:09:04 -0500

    Modificaciones para avanzar en el tutorial

--- jucatest/ContentView.swift
+++ jucatest/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world juca!")
-            .padding()
+        VStack{
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Nightly Tasks")
+                        .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.heavy)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .underline()
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    Text("Check All Windows")
+                    Text("Check all doors")
+                    Text("Check htat the safe is locked")
+                    Text("Check the mailbox")
+                    Text("Inspect security cameras")
+                    Text("Clear ice from sidewalks")
+                    Text("Document \"Strnge and unusual\" occurences")
+                    Text("Weekly Tasks").font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/).fontWeight(.heavy).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).underline()
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    Text("Monthly Tasks").font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/).fontWeight(.heavy).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).underline()
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                }
+                .foregroundColor(.gray)
+                Spacer()
+            }
+            .padding([.top,.leading])
+            Spacer()
+        }
     }
 }
 
